302fae1c97f86a6049806054585cea88dd49731e
mya63 <muhammedyunusamini4@gmail.com>
2026-01-29 14:24:17 +0100

    Notemodel, migrations, Api endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import health
+from .views import health, notes_list
 
 urlpatterns = [
-  path("health/", health)
+  path("health/", health),
+  path("notes/", notes_list),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,15 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Note
+from .serializers import NoteSerializer
 
 def health(request):
   return JsonResponse({"status": "ok"})
+
+
+@api_view(["GET"])
+def notes_list(request):
+  notes = Note.objects.all()
+  serializer = NoteSerializer(notes, many=True)
+  return Response(serializer.data)
