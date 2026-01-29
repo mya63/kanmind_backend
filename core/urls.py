c540b9ae30c07d386d7a67a6e11754ef1accdb96
mya63 <muhammedyunusamini4@gmail.com>
2026-01-29 19:53:57 +0100

    200,201,404

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import health, notes_list
+from .views import health, notes_list, note_detail
 
 urlpatterns = [
   path("health/", health),
   path("notes/", notes_list),
+  path("notes/<int:pk>/", note_detail),
 ]
--- core/views.py
+++ core/views.py
@@ -34,7 +34,7 @@ def note_detail(request, pk):
   except Note.DoesNotExist:
       return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
   
-  if request.method =="Get":
+  if request.method =="GET":
      serializer = NoteSerializer(note)
      return Response(serializer.data)
   
@@ -43,7 +43,8 @@ def note_detail(request, pk):
      if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
-     return Response(serializer.erros, status.HTTP_400_BAD_REQUEST)
+     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
-  note.delete()
-  return Response(status=status.HTTP_204_NO_CONTENT)
+  elif request.method =="DELETE":
+    note.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
