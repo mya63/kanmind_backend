3abf63ec93b5889eb0ca12af8da78548c0f751ad
mya63 <muhammedyunusamini4@gmail.com>
2026-01-29 13:46:25 +0100

    makemigrations / Admin

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField(blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+  def _str_(self):
+    return self.title 
