315a22cd5228b006ad2491c24fc1962d2458b901
mya63 <muhammedyunusamini4@gmail.com>
2026-03-05 04:22:43 +0100

    Register Board model in Django admin

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+# boards/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from boards.models import Board
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for Board model.
+    """
+
+    list_display = ("id", "title", "owner", "created_at")
+    search_fields = ("title", "owner__username", "owner__email")
+    list_filter = ("created_at",)
